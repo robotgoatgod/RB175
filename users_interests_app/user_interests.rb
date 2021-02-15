3a64467ba21bbc300790001d0d7ed96c2f34f463
endrjuki <endrju.vilcmeiers@gmail.com>
2021-02-15 02:55:49 +0200

    refactor

--- users_interests_app/user_interests.rb
+++ users_interests_app/user_interests.rb
@@ -1,3 +1,6 @@
+# user_interests.rb
+require "yaml"
+
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
@@ -7,31 +10,26 @@ before do
   @data = Psych.load_file("./data/users.yaml")  
 end
 
+helpers do
+  def count_interests(data)
+    data.reduce(0) do |sum, (name, user)|
+      sum + user[:interests].size
+    end
+  end
+
+  def user_count(data)
+    data.keys.count
+  end
+end
+
 get "/" do  
   erb :home
 end
 
-get "/users/:user" do
-  @name = params[:user]
-  @user_data = @data[@name.to_sym]
+get "/users/:user_name" do
+  @user_name = params[:user_name].to_sym
+  @email = @data[@user_name][:email]
+  @interests = @data[@user_name][:interests]
+
   erb :user
-end
-
-helpers do
-  def user_count(data)
-    data.keys.count
-  end
-
-  def interest_count(data)
-    data.values.reduce(0) { |total, details| total + details[:interests].size }
-  end
-
-  def display_other_user_links(data, current_user)
-    users = @data.keys
-    users.delete(current_user.to_sym)
-
-    users.map do |user|
-      %(<li><a href="/users/#{user}">#{user}</a></li>)
-    end.join("\n\n")
-  end
 end
